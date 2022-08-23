86643d19fdb439936941be029f379313905c905d
PercTruman <drshawnhart@gmail.com>
2022-08-23 14:48:06 -0500

    lab complete

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,29 +1,28 @@
 class RecipesController < ApplicationController
+    before_action :authorize
     
     def index
         recipes = Recipe.all
-        if @current_user
-            render json: recipes, include: [:user], status: :created
-        else
-            render json: {error: "Unauthorized"}, status: :unauthorized
-        end
+        render json: recipes, include: [:user], status: :created
     end
 
     def create
-        if @current_user
-            recipe = Recipe.create(recipe_params)
+            user = User.find_by(id: session[:user_id])
+            recipe = user.recipes.create(recipe_params)
             if recipe.valid?
                 render json: recipe, include: [:user], status: :created
             else
                 render json: {errors: recipe.errors.full_messages}, status: :unprocessable_entity
             end
-        else
-            render json: {error: "Unauthorized"}, status: :unauthorized
-        end
+      
     end
 
 
     private
+    def authorize
+       return render json: {errors: ["Not authorized"]}, status: :unauthorized unless session.include? :user_id
+    end
+
     def recipe_params
         params.permit(:title, :instructions, :minutes_to_complete)
     end
